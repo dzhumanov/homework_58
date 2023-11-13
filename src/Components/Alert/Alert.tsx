9cb5d26b45b62fd802038192f5bdf2e165118333
Temirlan Dzhumanov <temadjumanov@gmail.com>
2023-11-14 02:04:57 +0600

    Added little animation on modal and alert

--- src/Components/Alert/Alert.tsx
+++ src/Components/Alert/Alert.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { CSSTransition } from "react-transition-group";
+
 interface Props extends React.PropsWithChildren {
   show: boolean;
   type: string;
@@ -6,17 +9,21 @@ interface Props extends React.PropsWithChildren {
 
 const Alert: React.FC<Props> = ({ show, type, onDismiss, children }) => {
   return (
-    <div
-      className={`alert alert-${type} position-fixed top-25 start-50 translate-middle w-50 customAlert`}
-      style={{ display: show ? "block" : "none" }}
-    >
-      <div className="row">
-        <div className="col">{children}</div>
-        {onDismiss && (
-          <button className="btn-close col-3" onClick={onDismiss}></button>
-        )}
-      </div>
-    </div>
+    <>
+      <CSSTransition in={show} timeout={300} classNames="compon" unmountOnExit>
+        <div
+          className={`alert alert-${type} position-fixed top-25 start-50 translate-middle w-50 customAlert`}
+          style={{ display: show ? "block" : "none" }}
+        >
+          <div className="row">
+            <div className="col">{children}</div>
+            {onDismiss && (
+              <button className="btn-close col-3" onClick={onDismiss}></button>
+            )}
+          </div>
+        </div>
+      </CSSTransition>
+    </>
   );
 };
 
--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,9 +1,12 @@
+import React from "react";
+import { CSSTransition } from "react-transition-group";
 import Backdrop from "../Backdrop/Backdrop";
+import "./Modal.css";
 
 interface buttonConfig {
-    type: string;
-    label: string;
-    onClick: () => void;
+  type: string;
+  label: string;
+  onClick: () => void;
 }
 
 interface Props extends React.PropsWithChildren {
@@ -13,26 +16,45 @@ interface Props extends React.PropsWithChildren {
   buttons: buttonConfig[];
 }
 
-const Modal: React.FC<Props> = ({ show, title, onClose, buttons, children }) => {
+const Modal: React.FC<Props> = ({
+  show,
+  title,
+  onClose,
+  buttons,
+  children,
+}) => {
   return (
     <>
       <Backdrop show={show} onClick={onClose} />
-      <div className="modal show" style={{ display: show ? "block" : "none" }}>
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5">{title}</h1>
-              <button className="btn fw-bold fs-3 p-0" onClick={onClose}>x</button>
-            </div>
-            {children}
-            <div className="modal-footer">
-              {buttons.map((buttonItem) => (
-                  <button key={buttonItem.label} className={`btn btn-${buttonItem.type}`} onClick={buttonItem.onClick}>{buttonItem.label}</button>
-              ))}
+      <CSSTransition in={show} timeout={300} classNames="compon" unmountOnExit>
+        <div
+          className="modal show"
+          style={{ display: show ? "block" : "none" }}
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5">{title}</h1>
+                <button className="btn fw-bold fs-3 p-0" onClick={onClose}>
+                  x
+                </button>
+              </div>
+              {children}
+              <div className="modal-footer">
+                {buttons.map((buttonItem) => (
+                  <button
+                    key={buttonItem.label}
+                    className={`btn btn-${buttonItem.type}`}
+                    onClick={buttonItem.onClick}
+                  >
+                    {buttonItem.label}
+                  </button>
+                ))}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </CSSTransition>
     </>
   );
 };
